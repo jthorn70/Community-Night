e6402839cd066a001a0f6c683cc8d680f0320ccc
Jeremiah Thorn <jthorn70@gmail.com>
2023-04-09 18:10:22 -0400

    HomePage addition
    -added Coutdown clock and stream link to home page

---
+++ components/CountdownClock.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import { Grid, Text } from "@nextui-org/react";
+
+
+const CountdownClock = ({ targetDate }) => {
+    const getTimeRemaining = () => {
+        const total = Date.parse(targetDate) - Date.parse(new Date());
+        const seconds = Math.floor((total / 1000) % 60).toString().padStart(2, '0');
+        const minutes = Math.floor((total / 1000 / 60) % 60).toString().padStart(2, '0');
+        const hours = Math.floor((total / (1000 * 60 * 60)) % 24).toString().padStart(2, '0');
+        const days = Math.floor(total / (1000 * 60 * 60 * 24)).toString().padStart(2, '0');
+        return {
+            total,
+            days,
+            hours,
+            minutes,
+            seconds,
+        };
+    };
+
+    const [timeRemaining, setTimeRemaining] = useState(getTimeRemaining());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimeRemaining(getTimeRemaining());
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [targetDate]);
+
+    useEffect(() => {
+        const timerText = document.getElementById('timer-text');
+        if (timerText) {
+            timerText.textContent = `${timeRemaining.days}:${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`;
+        }
+    }, [timeRemaining]);
+
+    return (
+        <Grid.Container justify='center' gap={2}>
+            <Grid justify='center' xs={12}>
+                <Text h2>Community Night 1</Text>
+            </Grid>
+            <Grid justify='center' xs={12}>
+                <div id='timer-text' style={{ fontSize: '24px' }}>
+                    {timeRemaining.days}:{timeRemaining.hours}:{timeRemaining.minutes}:{timeRemaining.seconds}
+                </div>
+            </Grid>
+        </Grid.Container>
+    );
+};
+
+
+export default CountdownClock;
--- components/Home.js
+++ components/Home.js
@@ -1,16 +1,31 @@
-import { Image } from "@nextui-org/react";
+import { Grid, Image } from "@nextui-org/react";
+import dynamic from 'next/dynamic';
+import CountdownClock from "./CountdownClock";
 
 export default function Home() {
+    const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
     return (
-        <div style={{ width: "100%", height: "100vh" }}>
-            <Image
-                showSkeleton
-                width="100%"
-                height="100%"
-                maxDelay={10000}
-                src="https://img5.goodfon.com/original/2550x1800/8/1a/shtory-krasnye-zanaves.jpg"
-                alt="Default Image"
-            />
-        </div>
+        // <div style={{ width: "100%", height: "100vh" }}>
+        //     <Image
+        //         showSkeleton
+        //         width="100%"
+        //         height="100%"
+        //         maxDelay={10000}
+        //         src="https://img5.goodfon.com/original/2550x1800/8/1a/shtory-krasnye-zanaves.jpg"
+        //         alt="Default Image"
+        //     />
+        // </div>
+
+        <Grid.Container justify="center" gap={2}>
+            <CountdownClock targetDate={'2023-04-15T12:00:00Z'}></CountdownClock>
+            <ReactPlayer
+                url="https://www.twitch.tv/jbooogie"
+                controls
+                width={640 * 2}
+                height={360 * 2}>
+
+            </ReactPlayer>
+
+        </Grid.Container>
     );
 }
