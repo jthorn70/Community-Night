58a92b36e96cf0fce5f0c45b23381cd2cf40046a
Jeremiah Thorn <jthorn70@gmail.com>
2023-04-10 19:42:53 -0400

    updated countdown timer

--- components/Home.js
+++ components/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
     return (
 
         <Grid.Container justify="center" gap={2}>
-            <CountdownClock targetDate={'2023-04-14T16:00:00Z'}></CountdownClock>
+            <CountdownClock targetDate={'2023-04-15T00:00:00Z'}></CountdownClock>
             <ReactPlayer
                 url="https://www.twitch.tv/jbooogie"
                 controls
